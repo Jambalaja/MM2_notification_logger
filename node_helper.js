fa1c54696e1a6e366b5072d9d123e88cc6ea1ebd
aderiglasow <a.deriglasow@uni-bielefeld.de>
2019-04-03 18:26:29 +0200

    Notification Logger

---
+++ node_helper.js
@@ -0,0 +1,56 @@
+'use strict';
+const NodeHelper = require('node_helper');
+
+const {PythonShell} = require("python-shell");
+var pythonStarted = false;
+
+module.exports = NodeHelper.create(
+{
+	python_start: function () 
+	{
+		const self = this;		
+		self.pyshell = new PythonShell('modules/' + this.name + 
+		'/python-scripts/notification-logger.py', {pythonPath: 'python', args: [JSON.stringify(this.config)]});
+		console.log("notification-logger started");
+		self.pyshell.on('message', function (message)
+		{
+			console.log("notification-logger: " + message);
+		});
+  	},
+
+  	socketNotificationReceived: function(notification, payload) 
+	{
+		const self = this;
+		if(notification === 'CONFIG') 
+		{
+      			this.config = payload;
+			if(!pythonStarted)
+			{
+				pythonStarted = true;
+				this.python_start();
+			}
+		}
+		else
+		{
+			this.pyshell.send(payload);
+			//console.log("Send: " + entryString);
+		}
+  	},
+	
+	//call python clean up function
+	stop: function() 
+	{
+		const self = this;
+		self.pyshell.send('stop logging');
+		//self.pyshell.childProcess.kill('SIGKILL'); //process ends naturally, no need to kill
+		self.pyshell.end(function (err) 
+		{
+           		if (err)
+			{
+        			//throw err;
+    			};
+    			console.log('Clean up notification-logger finished');
+		});
+	}
+})
+
---
+++ python-scripts/notification-logger.py
@@ -0,0 +1,38 @@
+import threading
+import sys
+import os
+import json
+
+i = 0
+
+def to_node(message):
+	print(message)
+	# stdout is buffered and has to be flushed manually to prevent delays in the node helper communication
+	sys.stdout.flush()
+
+
+def check_stdin():
+	global i
+	while True:
+		lines = sys.stdin.readline()
+		if "stop logging" in lines:
+			close_log_file()
+			break
+		write_log(lines)
+		i += 1
+
+def write_log(logEntry):
+	#to_node("Writing: " + logEntry + "to " + logFile.name)
+	logFile.write(logEntry)
+
+def close_log_file():
+	logFile.close()
+	to_node("Logfile saved to " + os.path.realpath(logFile.name))
+	to_node(str(i) + " new entries saved")
+
+if __name__ == "__main__":
+	config = json.loads(sys.argv[1])
+	to_node("open log file " + config['logFilePath'] + " in mode '" + config['writeMode'] + "'")
+	logFile = open(config['logFilePath'], config['writeMode'])
+	t = threading.Thread(target=check_stdin)
+	t.start()
